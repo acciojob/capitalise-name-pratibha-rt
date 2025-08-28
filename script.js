f9cd20ffef5718742e5398fabd09e3569abb0cc6
pratibha-rt <pratibha.sept19@gmail.com>
2025-08-28 19:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function toUpperCaseField() {
+			let input = document.getElementById("fname");
+		    input.value = input.value.toUpperCase();
+		}
+
+		document.getElementById("fname").addEventListener("blur", toUpperCaseField);
